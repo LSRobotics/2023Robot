0eb166cce51be0d7dbafe20050c3ed65f7fffe4b
satrams-lschs <58091206+satrams-lschs@users.noreply.github.com>
2023-02-11 12:22:58 -0500

    Created ArmComponent class

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -2,16 +2,38 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.ArmConstants;
 
 public class ArmSubsystem extends SubsystemBase {
     
-    private WPI_TalonSRX upperArmMotor = new WPI_TalonSRX(Constants.ArmConstants.UpperArm.motor_id);
-    private WPI_TalonSRX lowerArmMotor = new WPI_TalonSRX(Constants.ArmConstants.LowerArm.motor_id);
-    
+    private ArmComponent upperArm = new ArmComponent(
+        ArmConstants.UpperArm.motor_id,
+        ArmConstants.UpperArm.kP,
+        ArmConstants.UpperArm.kI,
+        ArmConstants.UpperArm.kD
+    );
+
+    private ArmComponent lowerArm = new ArmComponent(
+        ArmConstants.LowerArm.motor_id,
+        ArmConstants.LowerArm.kP,
+        ArmConstants.LowerArm.kI,
+        ArmConstants.LowerArm.kD
+    );
+
     public ArmSubsystem() {
         super();
     }
 
+}
+
+class ArmComponent {
+    private WPI_TalonSRX motor;
+    private PIDController pid;
+
+    public ArmComponent(int motor_id, double kP, double kI, double kD) {
+        motor = new WPI_TalonSRX(motor_id);
+        pid = new PIDController(kP, kI, kD);
+    }
 }
