6a611b4962afe1989eaaca7122c9f07d6206b594
satrams-lschs <58091206+satrams-lschs@users.noreply.github.com>
2023-02-11 12:47:54 -0500

    Fleshed Out ArmComponent class

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
@@ -26,14 +27,43 @@ public class ArmSubsystem extends SubsystemBase {
         super();
     }
 
+    public void setArmPIDPositions(double lowerArmValue, double upperArmValue) {
+        lowerArm.setPIDPosition(lowerArmValue);
+        upperArm.setPIDPosition(upperArmValue);
+    }
+
+    @Override
+    public void periodic() {
+      // This method will be called once per scheduler run
+      lowerArm.pidCalculate();
+      upperArm.pidCalculate();
+    }
+
 }
 
 class ArmComponent {
     private WPI_TalonSRX motor;
     private PIDController pid;
+    private double pidValue = 0;
 
     public ArmComponent(int motor_id, double kP, double kI, double kD) {
         motor = new WPI_TalonSRX(motor_id);
         pid = new PIDController(kP, kI, kD);
+    }
+
+    public void setPIDPosition(double value) {
+        pid.setSetpoint(value);
+    }
+
+    public void pidCalculate() {
+        pidValue = MathUtil.clamp(pid.calculate(motor.getSelectedSensorPosition()), -.5, .5);
+    }
+
+    public void setPIDspeed() {
+        motor.set(pidValue);
+    }
+
+    public void setManualSpeed(double speed) {
+        motor.set(speed);
     }
 }
