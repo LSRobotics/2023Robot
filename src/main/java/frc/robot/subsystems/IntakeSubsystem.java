5403dd759d1f8d38be00ee3cbbd7f73c9587b20e
EvanComiciotto-lschs <72627874+EvanComiciotto-lschs@users.noreply.github.com>
2023-02-17 14:05:30 -0500

    Changed Intake from talon to NEO

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,9 +2,14 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.CANSparkMax;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import frc.robot.Constants;
 import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMax.IdleMode;
 
 public class IntakeSubsystem extends SubsystemBase {
     
@@ -13,11 +18,14 @@ public class IntakeSubsystem extends SubsystemBase {
     }
     private WPI_TalonSRX intake1 = new WPI_TalonSRX(21);
     private WPI_TalonSRX intake2 = new WPI_TalonSRX(22);
+
+    private CANSparkMax intakeMotor = new CANSparkMax(15, CANSparkMaxLowLevel.MotorType.kBrushless);
+
     private double intakePowerScalar = Constants.IntakeConstants.intake_default_speed;
     
     public void setPower(double power) {
-        intake1.set(ControlMode.PercentOutput, power * intakePowerScalar);
-        intake2.set(ControlMode.PercentOutput, -power * intakePowerScalar);
+        intakeMotor.set(power);
+        //intake2.set(ControlMode.PercentOutput, -power * intakePowerScalar);
     }
 
     public void setPowerScalar(double scalar) {
