301cb795e42b7201b4586c63c1730cfd3f3aefc5
La Salle Robotics Laptop <adilkhan2023@lschs.org>
2023-02-18 14:47:07 -0500

    Changed logic to work better again

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -54,23 +54,24 @@ public final class Constants {
     public static final double minEncoderAngle = -90;
     public static final double maxEncoderAngle = 90;
     public static final double encoderUnitsPerRevolution = 4096;
-    public static final double lowerArmSpeed = 0.2;
-    public static final double upperArmSpeed = 0.2;
 
     public static class UpperArm {
       public static final int motor_id = 31;
     
-      public static final double kP = 0;
+      public static final double kP = .1;
       public static final double kI = 0;
       public static final double kD = 0;
+      public static final double speedScalar = 0.2;
 
     }
     public static class LowerArm {
       public static final int motor_id = 32;
 
-      public static final double kP = 0;
+      public static final double kP = .1;
       public static final double kI = 0;
       public static final double kD = 0;
+      public static final double speedScalar = 0.2;
+
 
     }
   }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -64,42 +64,25 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-
-/* 
-    Trigger rightTriggerToggle = new Trigger(() -> {
-      return m_operatorController.getRightTriggerAxis() > 0.5;
-    });
-    Trigger leftTriggerToggle = new Trigger(() -> {
-      return m_operatorController.getLeftTriggerAxis() > 0.5;
-    });
-  */
-/*
-    rightTriggerToggle
-      .onTrue(Commands.runOnce(() -> m_IntakeSubsystem.setPowerScalar(Constants.IntakeConstants.intake_fast_speed)));
-    leftTriggerToggle
-      .onTrue(Commands.runOnce(() -> m_IntakeSubsystem.setPowerScalar(Constants.IntakeConstants.intake_slow_speed)));
-
-    leftTriggerToggle.or(rightTriggerToggle).onFalse(Commands.runOnce(() -> m_IntakeSubsystem.setPowerScalar(Constants.IntakeConstants.intake_default_speed)));
-
     
-    m_operatorController.rightBumper()
-        .onTrue(Commands.runOnce(() -> m_IntakeSubsystem.setPower(.3)));
-    m_operatorController.leftBumper()
-        .onTrue(Commands.runOnce(() -> m_IntakeSubsystem.setPower(-.3)));
+    m_operatorController.leftTrigger() //intake slow
+      .onTrue(Commands.runOnce(() -> m_IntakeSubsystem.setPower(Constants.IntakeConstants.intake_slow_speed)));
+    m_operatorController.rightTrigger() //intake fast
+      .onTrue(Commands.runOnce(() -> m_IntakeSubsystem.setPower(Constants.IntakeConstants.intake_fast_speed)));
+    m_operatorController.rightBumper() //outtake (shoot object out of intake)
+      .onTrue(Commands.runOnce(() -> m_IntakeSubsystem.setPower(-Constants.IntakeConstants.intake_fast_speed)));
 
-    m_operatorController.rightBumper().or(m_driverController.leftBumper()).onFalse(Commands.runOnce(() -> m_IntakeSubsystem.setPower(0.0)));
-    */
-    m_operatorController.leftTrigger().whileTrue(Commands.startEnd( () -> m_IntakeSubsystem.setPower(.3), () -> m_IntakeSubsystem.setPower(0.0)));
-    m_operatorController.rightTrigger().whileTrue(Commands.startEnd( () -> m_IntakeSubsystem.setPower(.5), () -> m_IntakeSubsystem.setPower(0.0)));
-    m_operatorController.rightBumper().whileTrue(Commands.startEnd( () -> m_IntakeSubsystem.setPower(-.5), () -> m_IntakeSubsystem.setPower(0.0)));
-    //m_operatorController.a().onTrue(Commands.runOnce(() -> m_ArmSubsystem.setArmPIDAngles(0.0, 0.0)));
-    //m_operatorController.y().onTrue(Commands.runOnce(() -> m_ArmSubsystem.setArmPIDAngles(0.0, 0.0)));
-    m_operatorController.leftBumper().whileTrue(Commands.startEnd( () -> m_ArmSubsystem.setArmSpeed(0.2,0.2), () -> m_ArmSubsystem.setArmSpeed(0.0, 0.0)));
-    m_operatorController.leftBumper().whileTrue(Commands.startEnd( () -> m_ArmSubsystem.setArmPIDAngles(1024.0,1024.0), () -> m_ArmSubsystem.setArmPIDAngles(0.0, 0.0)));
+    m_operatorController.rightTrigger().or(m_driverController.leftTrigger()).or(m_operatorController.rightBumper()).onFalse(Commands.runOnce(() -> m_IntakeSubsystem.setPower(0.0)));
+
+    m_operatorController.a().onTrue(Commands.runOnce(() -> m_ArmSubsystem.setArmPIDAngles(90, 90)));
+    m_operatorController.b().onTrue(Commands.runOnce(() -> m_ArmSubsystem.setArmPIDAngles(0,0)));
+    
+    // m_operatorController.leftTrigger().whileTrue(Commands.startEnd( () -> m_IntakeSubsystem.setPower(.3), () -> m_IntakeSubsystem.setPower(0.0)));
+    // m_operatorController.rightTrigger().whileTrue(Commands.startEnd( () -> m_IntakeSubsystem.setPower(.5), () -> m_IntakeSubsystem.setPower(0.0)));
+    // m_operatorController.rightBumper().whileTrue(Commands.startEnd( () -> m_IntakeSubsystem.setPower(-.5), () -> m_IntakeSubsystem.setPower(0.0)));
+
+    //m_operatorController.leftBumper().whileTrue(Commands.startEnd( () -> m_ArmSubsystem.setArmSpeed(0.2,0.2), () -> m_ArmSubsystem.setArmSpeed(0.0, 0.0)));
+    //m_operatorController.leftBumper().whileTrue(Commands.startEnd( () -> m_ArmSubsystem.setArmPIDAngles(90,90), () -> m_ArmSubsystem.setArmPIDAngles(0.0, 0.0)));
 
   }
 
