29d83cadb051b667bf785196248f68aee3bea3e6
satrams-lschs <58091206+satrams-lschs@users.noreply.github.com>
2023-02-20 12:10:34 -0500

    Changed Drive Train to Talon SRX motors

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import java.util.function.DoubleSupplier;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -21,60 +22,52 @@ import frc.robot.Constants.DriveTrainConstants;
 
 public class DriveTrain extends SubsystemBase {
 
-  private CANSparkMax fl_motor = new CANSparkMax(MotorConstants.fl_motor_id, CANSparkMaxLowLevel.MotorType.kBrushless);
-  private CANSparkMax fr_motor = new CANSparkMax(MotorConstants.fr_motor_id, CANSparkMaxLowLevel.MotorType.kBrushless);
-  private CANSparkMax br_motor = new CANSparkMax(MotorConstants.br_motor_id, CANSparkMaxLowLevel.MotorType.kBrushless);
-  private CANSparkMax bl_motor = new CANSparkMax(MotorConstants.bl_motor_id, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private WPI_TalonSRX fl_motor = new WPI_TalonSRX(MotorConstants.fl_motor_id);
+  private WPI_TalonSRX fr_motor = new WPI_TalonSRX(MotorConstants.fr_motor_id);
+  private WPI_TalonSRX br_motor = new WPI_TalonSRX(MotorConstants.br_motor_id);
+  private WPI_TalonSRX bl_motor = new WPI_TalonSRX(MotorConstants.bl_motor_id);
 
-  private MotorControllerGroup left_motors  = new MotorControllerGroup(fl_motor, bl_motor);
+  private MotorControllerGroup left_motors = new MotorControllerGroup(fl_motor, bl_motor);
   private MotorControllerGroup right_motors = new MotorControllerGroup(fr_motor, br_motor);
 
   private DifferentialDrive drive_controller = new DifferentialDrive(right_motors, left_motors);
 
-  private PIDController drivePID = new PIDController(DriveTrainConstants.DrivePID.kP,DriveTrainConstants.DrivePID.kI, DriveTrainConstants.DrivePID.kP);
-  private PIDController turnPID = new PIDController(DriveTrainConstants.TurnPID.kP,DriveTrainConstants.TurnPID.kI,DriveTrainConstants.TurnPID.kD);
+  private PIDController drivePID = new PIDController(DriveTrainConstants.DrivePID.kP, DriveTrainConstants.DrivePID.kI,
+      DriveTrainConstants.DrivePID.kP);
+  private PIDController turnPID = new PIDController(DriveTrainConstants.TurnPID.kP, DriveTrainConstants.TurnPID.kI,
+      DriveTrainConstants.TurnPID.kD);
 
   /** Creates a new ExampleSubsystem. */
   public DriveTrain() {
     super();
 
-    //The loopRampRate could be made a constant
-    fl_motor.setOpenLoopRampRate(0.5);
-    fr_motor.setOpenLoopRampRate(0.5);
-    br_motor.setOpenLoopRampRate(0.5);
-    bl_motor.setOpenLoopRampRate(0.5);
-
     right_motors.setInverted(true);
-    fl_motor.setIdleMode(IdleMode.kBrake);
-    fr_motor.setIdleMode(IdleMode.kBrake);
-    br_motor.setIdleMode(IdleMode.kBrake);
-    bl_motor.setIdleMode(IdleMode.kBrake);
-    
-    //set the controller to understand continuous angle input
+    // set the controller to understand continuous angle input
     turnPID.enableContinuousInput(-180, 180);
   }
 
   private double getEncoderValue() {
-    //NOTE: IMPLEMENT THIS
+    // NOTE: IMPLEMENT THIS
     return 0;
   }
 
   private double getEncoderAngle() {
-    //NOTE: IMPLEMENT THIS
+    // NOTE: IMPLEMENT THIS
     return 0;
   }
 
   public class pidDrive extends CommandBase {
 
     public pidDrive(double distance) {
-      //NOTE: ACTUALLY CALCULATE DISTANCE
+      // NOTE: ACTUALLY CALCULATE DISTANCE
       drivePID.reset();
       drivePID.setSetpoint(distance);
     }
 
     @Override
     public void execute() {
-      double speed = MathUtil.clamp(drivePID.calculate(getEncoderValue()), -DriveTrainConstants.DrivePID.maxSpeed, DriveTrainConstants.DrivePID.maxSpeed);
+      double speed = MathUtil.clamp(drivePID.calculate(getEncoderValue()), -DriveTrainConstants.DrivePID.maxSpeed,
+          DriveTrainConstants.DrivePID.maxSpeed);
       drive_controller.arcadeDrive(speed, 0);
     }
 
@@ -85,21 +78,22 @@ public class DriveTrain extends SubsystemBase {
 
     @Override
     public void end(boolean interrupted) {
-      drive_controller.arcadeDrive(0,0);
+      drive_controller.arcadeDrive(0, 0);
       drivePID.reset();
     }
   }
 
   public class pidTurn extends CommandBase {
     public pidTurn(double angle) {
-      //NOTE: ACTUALLY CALCULATE DISTANCE
+      // NOTE: ACTUALLY CALCULATE DISTANCE
       turnPID.reset();
       turnPID.setSetpoint(angle);
     }
 
     @Override
     public void execute() {
-      double speed = MathUtil.clamp(turnPID.calculate(getEncoderAngle()), -DriveTrainConstants.TurnPID.maxSpeed, DriveTrainConstants.TurnPID.maxSpeed);
+      double speed = MathUtil.clamp(turnPID.calculate(getEncoderAngle()), -DriveTrainConstants.TurnPID.maxSpeed,
+          DriveTrainConstants.TurnPID.maxSpeed);
       drive_controller.arcadeDrive(0, speed);
     }
 
@@ -110,29 +104,35 @@ public class DriveTrain extends SubsystemBase {
 
     @Override
     public void end(boolean interrupted) {
-      drive_controller.arcadeDrive(0,0);
+      drive_controller.arcadeDrive(0, 0);
       turnPID.reset();
     }
   }
 
-  //This is the command for handling ArcadeDrive logic externally. It is located within this subsystem because only this subsystem accesses it.
+  // This is the command for handling ArcadeDrive logic externally. It is located
+  // within this subsystem because only this subsystem accesses it.
   public class ArcadeDriveCommand extends CommandBase {
 
-    //These need to be lambdas (DoubleSupplier functions) because they need to be called every loop, returning different values each time
+    // These need to be lambdas (DoubleSupplier functions) because they need to be
+    // called every loop, returning different values each time
     private final DoubleSupplier driveSpeed;
     private final DoubleSupplier turnSpeed;
 
     public ArcadeDriveCommand(DoubleSupplier speed, DoubleSupplier turn) {
-        driveSpeed = speed;
-        turnSpeed = turn;
+      driveSpeed = speed;
+      turnSpeed = turn;
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     public void execute() {
-      //This is only able to directly access drive_controller becuase it is a nested class
-      //NOTE: if this code is changed to no longer be a nested class (for accessing another subsystem or something), this will no longer be able to directly access the drive (you just need to pass the subsystem and write an accessor method then)
-      drive_controller.arcadeDrive(driveSpeed.getAsDouble()*.7, turnSpeed.getAsDouble()*.5);
+      // This is only able to directly access drive_controller becuase it is a nested
+      // class
+      // NOTE: if this code is changed to no longer be a nested class (for accessing
+      // another subsystem or something), this will no longer be able to directly
+      // access the drive (you just need to pass the subsystem and write an accessor
+      // method then)
+      drive_controller.arcadeDrive(driveSpeed.getAsDouble() * .7, turnSpeed.getAsDouble() * .5);
     }
 
     // Called once after isFinished returns true
@@ -141,5 +141,5 @@ public class DriveTrain extends SubsystemBase {
       drive_controller.arcadeDrive(0, 0);
     }
   }
-  
+
 }
