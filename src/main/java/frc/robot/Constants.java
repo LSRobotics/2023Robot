7ee3c0415c99571cbeb650feb747c338f7b3cec8
satrams-lschs <58091206+satrams-lschs@users.noreply.github.com>
2023-02-20 12:23:03 -0500

    Changed constants and cleaned up intakesubsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,18 +13,19 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
     public static final int kOperatorControllerPort = 1;
   }
 
   public static class MotorConstants {
-    public static final int fr_motor_id = 11;
-    public static final int fl_motor_id = 13;
-    public static final int br_motor_id = 12;
-    public static final int bl_motor_id = 14;
+    public static final int fr_motor_id = 5;
+    public static final int fl_motor_id = 4;
+    public static final int br_motor_id = 2;
+    public static final int bl_motor_id = 3;
   }
+
   public static class DriveTrainConstants {
     public static class DrivePID {
       public static final double maxSpeed = .5;
@@ -32,6 +33,7 @@ public final class Constants {
       public static final double kI = 0;
       public static final double kD = 0;
     }
+
     public static class TurnPID {
       public static final double maxSpeed = .5;
       public static final double kP = 0;
@@ -39,31 +41,33 @@ public final class Constants {
       public static final double kD = 0;
     }
   }
+
   public static class IntakeConstants {
-    public static final int intake1_id = 21;
-    public static final int intake2_id = 22;
+    public static final int motor_id = 15;
 
     public static final double intake_motor_speed = .5;
     public static final double intake_fast_speed = 1.0;
     public static final double intake_slow_speed = 0.5;
     public static final double intake_default_speed = 0.75;
   }
+
   public static class ArmConstants {
 
-    //TODO: Figure out all of these values
+    // TODO: Figure out all of these values
     public static final double minEncoderAngle = -90;
     public static final double maxEncoderAngle = 90;
     public static final double encoderUnitsPerRevolution = 4096;
 
     public static class UpperArm {
       public static final int motor_id = 31;
-    
+
       public static final double kP = .1;
       public static final double kI = 0;
       public static final double kD = 0;
       public static final double speedScalar = 0.1;
 
     }
+
     public static class LowerArm {
       public static final int motor_id = 32;
 
@@ -71,7 +75,6 @@ public final class Constants {
       public static final double kI = 0;
       public static final double kD = 0;
       public static final double speedScalar = 0.1;
-
 
     }
   }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,15 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import frc.robot.Constants;
 import frc.robot.Constants.IntakeConstants;
-import frc.robot.Constants;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
-import com.revrobotics.CANSparkMax.IdleMode;
 
 public class IntakeSubsystem extends SubsystemBase {
     
@@ -17,9 +11,9 @@ public class IntakeSubsystem extends SubsystemBase {
         super();
     }
 
-    private CANSparkMax intakeMotor = new CANSparkMax(15, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private CANSparkMax intakeMotor = new CANSparkMax(IntakeConstants.motor_id, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-    private double intakePowerScalar = Constants.IntakeConstants.intake_default_speed;
+    private double intakePowerScalar = IntakeConstants.intake_default_speed;
     
     public void setPower(double power) {
         intakeMotor.set(power);
