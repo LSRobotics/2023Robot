da989fdb80277608067b7432c12667a13c85eb92
satrams-lschs <58091206+satrams-lschs@users.noreply.github.com>
2023-03-01 16:00:14 -0500

    moved value to constants

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,12 +1,14 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class IntakeSubsystem extends SubsystemBase {
     
-    private WPI_TalonSRX intakeMotor = new WPI_TalonSRX(15);
+    private WPI_TalonSRX intakeMotor = new WPI_TalonSRX(IntakeConstants.motor_id);
     public IntakeSubsystem() {
         super();
         intakeMotor.setInverted(true);
