9f6210bb3b87ef9379eac2df1fa291b631c7a3d3
EvanComiciotto-lschs <72627874+EvanComiciotto-lschs@users.noreply.github.com>
2023-03-17 14:09:19 -0400

    Set up all the cone autons and they work
    yay

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -63,7 +63,7 @@ public final class Autos {
   }
 
 
-  public final static CommandBase placeCone(IntakeSubsystem intakeSubsystem, ArmSubsystem armSubsystem) {
+  public final static CommandBase placeConeAndExit(DriveTrain driveTrain, IntakeSubsystem intakeSubsystem, ArmSubsystem armSubsystem) {
     return Commands.sequence(
       Commands.race(
         Commands.startEnd(() -> {armSubsystem.setArmSpeed(ArmConstants.fast_speed);}, () -> {armSubsystem.setArmSpeed(0);}),
@@ -71,10 +71,10 @@ public final class Autos {
       ),
       new WaitCommand(0.5),
       Commands.race(
-        Commands.startEnd(() -> {intakeSubsystem.setPower(-1);}, () -> {intakeSubsystem.setPower(0);}, intakeSubsystem),
+        Commands.startEnd(() -> {intakeSubsystem.setPower(-0.7);}, () -> {intakeSubsystem.setPower(0);}, intakeSubsystem),
         new WaitCommand(0.5)
-      )
-      
+      ),
+      new pidDrive(-180, driveTrain)
     );
       
     
@@ -95,7 +95,7 @@ public final class Autos {
     );
   }
 
-  public final static CommandBase testLeavePlaceAndBalanceAuto(DriveTrain driveTrain, IntakeSubsystem intakeSubsystem, ArmSubsystem armSubsystem, LEDSubsystem ledSubsystem) {
+  public final static CommandBase testLeavePlaceAndBalanceAutoCube(DriveTrain driveTrain, IntakeSubsystem intakeSubsystem, ArmSubsystem armSubsystem, LEDSubsystem ledSubsystem) {
     return Commands.sequence(
       Commands.race(
         Commands.startEnd(() -> {armSubsystem.setArmSpeed(ArmConstants.fast_speed);}, () -> {armSubsystem.setArmSpeed(0);}),
@@ -104,6 +104,32 @@ public final class Autos {
       new WaitCommand(0.5),
       Commands.race(
         Commands.startEnd(() -> {intakeSubsystem.setPower(-0.7);}, () -> {intakeSubsystem.setPower(0);}, intakeSubsystem),
+        new WaitCommand(0.5)
+      ),
+      //Move and retract arm
+      Commands.parallel(
+        new pidDrive(-168, driveTrain),
+        Commands.race(
+          Commands.startEnd(() -> {armSubsystem.setArmSpeed(-ArmConstants.fast_speed);}, () -> {armSubsystem.setArmSpeed(0);}),
+          new WaitCommand(2)
+        )
+      ),
+      new moveUntilAngled(driveTrain, 1),
+      new AutoBalance(driveTrain),
+      new PrintCommand("finished"),
+      new StayStill(driveTrain, ledSubsystem)
+    );
+  }
+
+  public final static CommandBase testLeavePlaceAndBalanceAutoCone(DriveTrain driveTrain, IntakeSubsystem intakeSubsystem, ArmSubsystem armSubsystem, LEDSubsystem ledSubsystem) {
+    return Commands.sequence(
+      Commands.race(
+        Commands.startEnd(() -> {armSubsystem.setArmSpeed(ArmConstants.fast_speed);}, () -> {armSubsystem.setArmSpeed(0);}),
+        new WaitCommand(1.5)
+      ),
+      new WaitCommand(0.5),
+      Commands.race(
+        Commands.startEnd(() -> {intakeSubsystem.setPower(-1);}, () -> {intakeSubsystem.setPower(0);}, intakeSubsystem),
         new WaitCommand(0.5)
       ),
       //Move and retract arm
